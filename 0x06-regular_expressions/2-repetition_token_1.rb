8731bffc760fe6d47e92b6f0fabcea9cb51a73aa
Aahad Yamani <aahadyamani09@gmail.com>
2023-01-31 12:44:55 -0800

    update

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,8 +1,11 @@
 #!/usr/bin/env ruby
+#
+#require 'byebug'
+#
 #def match_regex(string)
 #  regex = /hb{0,4}tn/
+#  byebug # start the debugger session
 #  return string.match(regex) ? true : false
 #end
 #
 #puts match_regex(ARGV[0])
-#
